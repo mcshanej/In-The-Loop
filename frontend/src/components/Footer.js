d16161919f729095ac85a12a57620e15d6d158d4
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2021-04-12 17:31:47 -0700

    added footer to web app and some css

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+export default function Footer(){
+    return(
+            <div className="footer-container">
+                <h3>Coded By:</h3>
+                <a className="LinkedIn-profile-link JA" href="https://www.linkedin.com/in/juliette-alexandria-8946a91b0/" target="_blank" rel="external author">Juliette Alexandria</a>                        
+                <a className="LinkedIn-profile-link MM" href="https://www.linkedin.com/in/maria-martinez-8810081a2/" target="_blank" rel="external author">Maria Martinez</a>
+                <a className="LinkedIn-profile-link DT" href="https://www.linkedin.com/in/dawn-trimble-097a25b4/" target="_blank" rel="external author">Dawn Trimble</a>
+            </div>
+    )
+}
