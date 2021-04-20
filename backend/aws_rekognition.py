890b3088735a1ccbb1734db239f2a09e453431a7
Juliette McShane Alexandria <jmcshane@access2online.com>
2021-04-20 10:46:09 -0700

    Added facial detection with corresponding lambda event

--- backend/aws_rekognition.py
+++ backend/aws_rekognition.py
@@ -1,5 +1,4 @@
 #Modified from https://docs.aws.amazon.com/rekognition/latest/dg/labels-detect-labels-image.html to be used in a lambda fun tion and format returns differently.
-
 import json
 import boto3
 
@@ -13,12 +12,31 @@ def detect_labels(photo, bucket):
     print('Detected labels for ' + photo) 
     print()   
     for label in response['Labels']:
-        confidence = str(label['Confidence'])
+        confidence = label['Confidence']
         print ("Label: " + label['Name'])
-        print (f"Confidence: {confidence:.2f}%.")
-        print ()
+        print (f'Confidence: {confidence:.2f}%.')
+
     return len(response['Labels'])
+    
+def detect_faces(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
+
+    print('Detected faces for ' + photo)    
+    for faceDetail in response['FaceDetails']:
+        print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) 
+              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old, likely ' + str(faceDetail['Gender']['Value']) + '.')
+        print('Here are the other attributes:')
+        #referenced faceDetail before brackets and made sure to parse any number as an integer
+        for emotion in faceDetail['Emotions']:
+            if int(emotion['Confidence']) >= 50:
+                print('The person is likely ' + emotion['Type'] +'.')
+            return len(response['FaceDetails'])
+
 
 def lambda_handler(event, context):
     print("event", event)
     detect_labels(event['Records'][0]['s3']['object']['key'], 'intheloop')
+    detect_faces(event['Records'][0]['s3']['object']['key'], 'intheloop')
