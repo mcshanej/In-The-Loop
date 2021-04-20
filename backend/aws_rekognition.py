d40b552bfdd05a2157b5d2077d6053f64c0bfdb5
Juliette McShane Alexandria <jmcshane@access2online.com>
2021-04-19 17:31:17 -0700

    Formatted label output for confidence metric into 2 decimal place percentage'

--- backend/aws_rekognition.py
+++ backend/aws_rekognition.py
@@ -1,3 +1,5 @@
+#Modified from https://docs.aws.amazon.com/rekognition/latest/dg/labels-detect-labels-image.html to be used in a lambda fun tion and format returns differently.
+
 import json
 import boto3
 
@@ -6,13 +8,14 @@ def detect_labels(photo, bucket):
     client=boto3.client('rekognition')
 
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
-        MaxLabels=5)
+        MaxLabels=5, MinConfidence=80)
 
     print('Detected labels for ' + photo) 
     print()   
     for label in response['Labels']:
+        confidence = str(label['Confidence'])
         print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
+        print (f"Confidence: {confidence:.2f}%.")
         print ()
     return len(response['Labels'])
 
