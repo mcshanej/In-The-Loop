d48e6a12ad20934a9fb3d2e4779a9e9ffe2bd114
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2021-06-01 17:07:50 -0700

    added netlify folder to see if it works

---
+++ frontend/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+export default function Footer(){
+    return(
+        <div className="footer-container">
+            <h3>Coded By:</h3>
+            <a className="LinkedIn-profile-link JA" href="https://www.linkedin.com/in/juliette-alexandria-8946a91b0/" target="_blank" rel="noreferrer">Juliette Alexandria</a>                        
+            <a className="LinkedIn-profile-link MM" href="https://www.linkedin.com/in/maria-martinez-8810081a2/" target="_blank" rel="noreferrer">Maria Martinez</a>
+            <a className="LinkedIn-profile-link DT" href="https://www.linkedin.com/in/dawn-trimble-097a25b4/" target="_blank" rel="noreferrer">Dawn Trimble</a>
+        </div>
+    )
+}
--- frontend/src/components/DisplayInfo.js
+++ frontend/src/components/DisplayInfo.js
@@ -1,10 +1,14 @@
-import React,{ useState} from "react";
+import React,{ useState, Component} from "react";
 import DecodeBase64 from './DecodeBase64';
+import Table from 'react-bootstrap/Table';
 
 export default function DisplayInfo(props){
   const [information, setInfo] = useState([]);
   const [otherInfo, setOther] = useState([]);
- 
+
+  console.log(`this is infromation at inital state: ${JSON.stringify(information)}`);
+  console.log(`this is otherInfo at inital state: ${JSON.stringify(otherInfo)}`);
+
   function setInfoDisplay(){
     var tempObj ={
       name: props.value[0],
@@ -24,6 +28,7 @@ export default function DisplayInfo(props){
     })
       .then(res => res.json())
       .then(json => {
+        console.log(`This is JSON after api is called: ${JSON.stringify(json)}`);
         setInfo(json.labels);
         setOther(json.people);
 
@@ -34,21 +39,65 @@ export default function DisplayInfo(props){
       <>
         <button onClick={setInfoDisplay}>Upload</button> 
         <br/>
-        <DecodeBase64 base64={props.value[1]}/> 
-          {information.map((information)=>(
-            <div> 
-            <div>{information.confidence}</div>
-            <div>{information.name}</div>
-            </div>
-          ))}
-          {otherInfo.map((otherInfo)=>(
-            <div>
-            <div>{otherInfo.agemin}</div>
-            <div>{otherInfo.agemax}</div>
-            <div>{otherInfo.Gender}</div>
-            <div>{otherInfo.Emotions}</div>
-            </div>
-          ))}
-      </>
+        <br/>
+        <div className='displayInfo container'>
+          <DecodeBase64 base64={props.value[1]}/> 
+          {/* <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Name</th>
+                <th scope="col">Confidence Percentage</th>
+              </tr>
+            </thead>
+              {information.map((information)=>(
+                <tbody>
+                <tr>
+                  <th scope="row"></th>
+                  <td>{information.name}</td>
+                  <td>{Math.floor(information.confidence)}%</td>
+                </tr>
+                 </tbody>
+            ))}
+          </Table>
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Age Min</th>
+                <th scope="col">Age Max</th>
+                <th scope="col">Emotions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {otherInfo.map((otherInfo)=>(
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{otherInfo.agemin}</td>
+                  <td>{otherInfo.agemax}</td>
+                  <td>{otherInfo.Gender}</td>
+                  <td>{otherInfo.Emotions}</td>
+                </tr>
+            ))}
+            </tbody>
+          </Table> */}
+            {information.map((information)=>(
+              <div> 
+              <div>{Math.floor(information.confidence)}%</div>
+              <div>{information.name}</div>
+              </div>
+            ))} 
+            {otherInfo.map((otherInfo)=>(
+              <div>
+              <div>{otherInfo.agemin}</div>
+              <div>{otherInfo.agemax}</div>
+              <div>{otherInfo.Gender}</div>
+              <div>{otherInfo.Emotions}</div>
+              </div>
+            ))}
+        </div>
+        </>
+    
     )
 }
